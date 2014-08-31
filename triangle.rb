4c7a0e29fa06228cb7cde78044b320f852051e31
antmachine <anthonysallows@gmail.com>
2014-08-30 17:18:02 -0700

    Completed about_triangle_project

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if(a == b && b == c)
+  	return :equilateral
+  elsif(a == b || b == c || a == c)
+  	return :isosceles
+  else
+  	return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
